fa6a0fb2f3fbd96b3b1ea4ec1a492eabb05719a6
Edgar Vergara <edgarvm13@hotmail.com>
2024-04-21 16:08:55 +0200

    authentication using auth0

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import React from "react";
 import AppRoutes from "./AppRoutes";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
+import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
-      <AppRoutes />
+      <Auth0ProviderWithNavigate>
+        <AppRoutes />
+      </Auth0ProviderWithNavigate>
     </Router>
   </React.StrictMode>
 );
