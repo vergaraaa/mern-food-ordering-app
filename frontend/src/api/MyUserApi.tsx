03c5ce68347bd19e3fbf602093dfee3451e4df59
Edgar Vergara <edgarvm13@hotmail.com>
2024-04-22 19:10:01 +0200

    ✨ get current user data in frontend

--- frontend/src/api/MyUserApi.tsx
+++ frontend/src/api/MyUserApi.tsx
@@ -1,8 +1,46 @@
 import { toast } from "sonner";
-import { useMutation } from "react-query";
+import { useMutation, useQuery } from "react-query";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const useGetMyUser = () => {
+  const { getAccessTokenSilently } = useAuth0();
+
+  const getMyUserRequest = async () => {
+    const accessToken = await getAccessTokenSilently();
+
+    const response = await fetch(`${API_BASE_URL}/api/my-user`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to get user");
+    }
+
+    return await response.json();
+  };
+
+  const {
+    data: currentUser,
+    isLoading,
+    error,
+  } = useQuery("getMyCurrentUser", getMyUserRequest);
+
+  if (error) {
+    toast.error(error.toString());
+  }
+
+  return {
+    currentUser,
+    isLoading,
+    error,
+  };
+};
 
 type CreateUserRequest = {
   auth0Id: string;
