662d55ff613d659d30db6daeb97cef8efb5accc3
Edgar Vergara <edgarvm13@hotmail.com>
2024-04-28 16:50:01 +0200

    ✨ create order in database

--- backend/src/controllers/orders.ts
+++ backend/src/controllers/orders.ts
@@ -1,6 +1,7 @@
 import Stripe from "stripe";
 import { Request, Response } from "express";
 import Restaurant, { MenuItemType } from "../models/restaurant";
+import Order from "../models/order";
 
 const STRIPE = new Stripe(process.env.STRIPE_API_KEY as string);
 const FRONTEND_URL = process.env.FRONTEND_URL as string;
@@ -32,6 +33,15 @@ export const createCheckoutSession = async (req: Request, res: Response) => {
       throw new Error("Restaurant not found");
     }
 
+    const newOrder = new Order({
+      restaurant: restaurant,
+      user: req.userId,
+      status: "placed",
+      deliveryDetails: checkoutSessionRequest.deliveryDetails,
+      cartItems: checkoutSessionRequest.cartItems,
+      createdAt: new Date(),
+    });
+
     const lineItems = createLineItems(
       checkoutSessionRequest,
       restaurant.menuItems
@@ -39,7 +49,7 @@ export const createCheckoutSession = async (req: Request, res: Response) => {
 
     const session = await createSession(
       lineItems,
-      "TEST_ORDER_ID",
+      newOrder._id.toString(),
       restaurant.deliveryPrice,
       restaurant._id.toString()
     );
@@ -49,6 +59,8 @@ export const createCheckoutSession = async (req: Request, res: Response) => {
         message: "Error creating stripe session",
       });
     }
+
+    await newOrder.save();
 
     return res.status(200).json({
       url: session.url,
---
+++ backend/src/models/order.ts
@@ -0,0 +1,35 @@
+import { InferSchemaType, model, Schema, Types } from "mongoose";
+
+const orderSchema = new Schema({
+  restaurant: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: "Restaurant",
+  },
+  user: { type: Schema.Types.ObjectId, required: true, ref: "User" },
+  deliveryDetails: {
+    email: { type: String, required: true },
+    name: { type: String, required: true },
+    addressLine1: { type: String, required: true },
+    city: { type: String, required: true },
+  },
+  cartItems: [
+    {
+      menuItemId: { type: String, required: true },
+      quantity: { type: Number, required: true },
+      name: { type: String, required: true },
+    },
+  ],
+  totalAmount: Number,
+  status: {
+    type: String,
+    enum: ["placed", "paid", "inProgress", "outForDelivey", "delivererd"],
+  },
+  createdAt: { type: Date, default: Date.now },
+});
+
+export type OrderType = InferSchemaType<typeof orderSchema>;
+
+const Order = model("Order", orderSchema);
+
+export default Order;
